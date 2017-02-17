a401ff32fd619ba1d6acffe2af6e648a5cf5c9de
samussiah <samussiah@gmail.com>
2017-02-17 16:09:31 -0500

    Add charts.

--- src/chart/summaryTable/renderRow.js
+++ src/chart/summaryTable/renderRow.js
@@ -1,3 +1,5 @@
+import makeOverview from './renderRow/makeOverview.js';
+
 /*------------------------------------------------------------------------------------------------\
 intialize the summary table
 \------------------------------------------------------------------------------------------------*/
@@ -5,5 +7,5 @@ intialize the summary table
 export function renderRow(d) {
   var rowWrap = d3.select(this)
   rowWrap.append("div").attr("class","row-overview").call(makeOverview)
-  rowWrap.append("div").attr("class","row-details").call(makeDetails)
+  //rowWrap.append("div").attr("class","row-details").call(makeDetails)
 }
