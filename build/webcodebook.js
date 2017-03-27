51e11e57b747baaf205262f206c486e959b47d04
Jeremy Wildfire <jwildfire@gmail.com>
2017-03-27 15:50:42 -0700

    modularize render row a bit

--- build/webcodebook.js
+++ build/webcodebook.js
@@ -210,6 +210,81 @@ var webcodebook = function (webcharts) {
 		}
 	}
 
+	function makeDotPlot(this_, d) {
+		var chartContainer = d3.select(this_).node();
+		var chartSettings = { x: { column: 'prop_n',
+				type: 'linear',
+				label: '',
+				format: '%',
+				domain: [0, null] },
+			y: { column: 'key',
+				type: 'ordinal',
+				label: '' },
+			marks: [{ type: 'text',
+				text: "|",
+				attributes: { "font-weight": 'bold', "alignment-baseline": "middle" },
+				per: ['key'],
+				summarizeX: 'mean',
+				tooltip: '[key]: [n] ([prop_n])' }],
+			gridlines: 'xy',
+			resizable: false,
+			height: this_.height,
+			margin: this_.margin
+		};
+
+		var chartData = d.statistics.values.sort(function (a, b) {
+			return a.prop_n > b.prop_n ? -2 : a.prop_n < b.prop_n ? 2 : a.key < b.key ? -1 : 1;
+		}).slice(0, 5);
+		chartSettings.y.order = chartData.map(function (d) {
+			return d.key;
+		}).reverse();
+		console.log(d);
+		if (d.groups) {
+			chartData.forEach(function (di) {
+				return di.group = 'All';
+			});
+
+			d.groups.forEach(function (group) {
+				group.statistics.values.filter(function (value) {
+					return chartSettings.y.order.indexOf(value.key) > -1;
+				}).sort(function (a, b) {
+					return a.prop_n > b.prop_n ? -2 : a.prop_n < b.prop_n ? 2 : a.key < b.key ? -1 : 1;
+				}).forEach(function (value) {
+					value.group = group.group;
+					chartData.push(value);
+				});
+			});
+			console.log(chartData);
+			chartSettings.marks[0].per.push('group');
+			chartSettings.marks[0].values = { 'group': ['All'] };
+			chartSettings.marks.push({ type: 'circle',
+				per: ['key', 'group'],
+				summarizeX: 'mean',
+				radius: 3,
+				values: { 'group': d.groups.map(function (d) {
+						return d.group;
+					}) } });
+			chartSettings.color_by = 'group';
+			chartSettings.legend = { label: '',
+				order: d.groups.map(function (d) {
+					return d.group;
+				}),
+				mark: "circle"
+			};
+			console.log(d.groups.map(function (d) {
+				return d.group;
+			}));
+		}
+
+		var chart = webCharts.createChart(chartContainer, chartSettings);
+		chart.on("resize", function () {
+			var allLegend = this.wrap.select("ul.legend").select("li");
+			allLegend.select("svg").remove();
+			allLegend.select("span.legend-mark-text").text("|").style("color", "black").style("font-weight", "bolder");
+		});
+		chart.init(chartData);
+	}
+
 	if (typeof Object.assign != 'function') {
 		(function () {
 			Object.assign = function (target) {
@@ -703,109 +778,42 @@ var webcodebook = function (webcharts) {
 		return chart;
 	}
 
-	function makeChart(d, group) {
+	function makeHistogram(this_, d) {
+		var chartContainer = d3.select(this_).node();
+		var chartSettings = { measure: ' ',
+			resizable: false,
+			height: 100,
+			margin: this_.margin };
+		var chartData = [];
+
+		if (d.groups) {
+			chartSettings.panel = 'group';
+			d.groups.forEach(function (group) {
+				group.values.forEach(function (value) {
+					chartData.push({ group: group.group, ' ': value });
+				});
+			});
+		} else {
+			d.values.forEach(function (d) {
+				chartData.push({ ' ': d });
+			});
+		}
+
+		var chart = createHistogram(chartContainer, chartSettings);
+		chart.init(chartData);
+	}
+
+	function makeChart(d) {
 		//Common chart settings
-		var margin = {};
-		var height = 100;
+		this.margin = {};
+		this.height = 100;
 
 		if (d.type === 'categorical') {
 			// categorical outcomes
-			console.log(d);
-			var chartContainer = d3.select(this).node();
-			var chartSettings = { x: { column: 'prop_n',
-					type: 'linear',
-					label: '',
-					format: '%',
-					domain: [0, null] },
-				y: { column: 'key',
-					type: 'ordinal',
-					label: '' },
-				marks: [{ type: 'text',
-					text: "|",
-					attributes: { "font-weight": 'bold', "alignment-baseline": "middle" },
-					per: ['key'],
-					summarizeX: 'mean',
-					tooltip: '[key]: [n] ([prop_n])' }],
-				gridlines: 'xy',
-				resizable: false,
-				height: height,
-				margin: margin
-			};
-			var chartData = d.statistics.values.sort(function (a, b) {
-				return a.prop_n > b.prop_n ? -2 : a.prop_n < b.prop_n ? 2 : a.key < b.key ? -1 : 1;
-			}).slice(0, 5);
-			chartSettings.y.order = chartData.map(function (d) {
-				return d.key;
-			}).reverse();
-			console.log(d);
-			if (d.groups) {
-				chartData.forEach(function (di) {
-					return di.group = 'All';
-				});
-
-				d.groups.forEach(function (group) {
-					group.statistics.values.filter(function (value) {
-						return chartSettings.y.order.indexOf(value.key) > -1;
-					}).sort(function (a, b) {
-						return a.prop_n > b.prop_n ? -2 : a.prop_n < b.prop_n ? 2 : a.key < b.key ? -1 : 1;
-					}).forEach(function (value) {
-						value.group = group.group;
-						chartData.push(value);
-					});
-				});
-				console.log(chartData);
-				chartSettings.marks[0].per.push('group');
-				chartSettings.marks[0].values = { 'group': ['All'] };
-				chartSettings.marks.push({ type: 'circle',
-					per: ['key', 'group'],
-					summarizeX: 'mean',
-					radius: 3,
-					values: { 'group': d.groups.map(function (d) {
-							return d.group;
-						}) } });
-				chartSettings.color_by = 'group';
-				chartSettings.legend = { label: '',
-					order: d.groups.map(function (d) {
-						return d.group;
-					}),
-					mark: "circle"
-				};
-				console.log(d.groups.map(function (d) {
-					return d.group;
-				}));
-			}
-
-			var chart = webCharts.createChart(chartContainer, chartSettings);
-			chart.on("resize", function () {
-				var allLegend = this.wrap.select("ul.legend").select("li");
-				allLegend.select("svg").remove();
-				allLegend.select("span.legend-mark-text").text("|").style("color", "black").style("font-weight", "bolder");
-			});
-			chart.init(chartData);
+			makeDotPlot(this, d);
 		} else {
 			// continuous outcomes
-			var _chartContainer = d3.select(this).node();
-			var _chartSettings = { measure: ' ',
-				resizable: false,
-				height: 100,
-				margin: margin };
-			var _chartData = [];
-
-			if (d.groups) {
-				_chartSettings.panel = 'group';
-				d.groups.forEach(function (group) {
-					group.values.forEach(function (value) {
-						_chartData.push({ group: group.group, ' ': value });
-					});
-				});
-			} else {
-				d.values.forEach(function (d) {
-					_chartData.push({ ' ': d });
-				});
-			}
-
-			var _chart = createHistogram(_chartContainer, _chartSettings);
-			_chart.init(_chartData);
+			makeHistogram(this, d);
 		}
 	}
 
--- src/chart/summaryTable/renderRow/makeChart.js
+++ src/chart/summaryTable/renderRow/makeChart.js
@@ -1,109 +1,14 @@
-import { createHistogram } from '../../../createHistogram.js'
+import { makeDotPlot } from './makeDotPlot.js'
+import { makeHistogram } from './makeHistogram.js'
 
-export default function makeChart(d, group) {
+export default function makeChart(d) {
   //Common chart settings
-    const margin = {};
-    const height = 100;
+    this.margin = {};
+    this.height = 100;
 
     if (d.type === 'categorical') { // categorical outcomes
-        console.log(d)
-        const chartContainer = d3.select(this).node();
-        const chartSettings =
-            {x: {column: 'prop_n'
-                ,type: 'linear'
-                ,label: ''
-                ,format: '%'
-                ,domain: [0,null]}
-            ,y: {column: 'key'
-                ,type: 'ordinal'
-                ,label: ''}
-            ,marks:
-                [   {type: 'text'
-                    ,text:"|"
-                    ,attributes:{"font-weight":'bold', "alignment-baseline":"middle"}
-                    ,per: ['key']
-                    ,summarizeX: 'mean'
-                    ,tooltip: '[key]: [n] ([prop_n])'}
-                ]
-            ,gridlines: 'xy'
-            ,resizable: false
-            ,height: height
-            ,margin: margin
-            };
-        const chartData = d.statistics.values
-            .sort((a,b) =>
-                a.prop_n > b.prop_n ? -2 :
-                a.prop_n < b.prop_n ?  2 :
-                    a.key < b.key ? -1 : 1)
-            .slice(0,5);
-        chartSettings.y.order = chartData.map(d => d.key).reverse();
-        console.log(d)
-        if (d.groups) {
-          chartData.forEach(di => di.group = 'All');
-
-            d.groups.forEach(group => {
-                group.statistics.values
-                    .filter(value => chartSettings.y.order.indexOf(value.key) > -1)
-                    .sort((a,b) =>
-                        a.prop_n > b.prop_n ? -2 :
-                        a.prop_n < b.prop_n ?  2 :
-                            a.key < b.key ? -1 : 1)
-                    .forEach(value => {
-                        value.group = group.group;
-                        chartData.push(value);
-                    });
-            });
-            console.log(chartData)
-            chartSettings.marks[0].per.push('group');
-            chartSettings.marks[0].values = {'group': ['All']};
-            chartSettings.marks.push(
-                {type: 'circle'
-                ,per: ['key', 'group']
-                ,summarizeX: 'mean'
-                ,radius: 3
-                ,values: {'group': d.groups.map(d => d.group)}});
-            chartSettings.color_by = 'group';
-            chartSettings.legend =
-              {label: ''
-                ,order: d.groups.map(d => d.group)
-                ,mark:"circle"
-              };
-              console.log(d.groups.map(d => d.group))
-        }
-
-        const chart = webCharts.createChart(chartContainer, chartSettings);
-        chart.on("resize",function(){
-          var allLegend = this.wrap.select("ul.legend").select("li")
-          allLegend.select("svg").remove()
-          allLegend.select("span.legend-mark-text")
-          .text("|")
-          .style("color","black")
-          .style("font-weight","bolder")
-        })
-        chart.init(chartData);
+      makeDotPlot(this,d)
     } else { // continuous outcomes
-        const chartContainer = d3.select(this).node();
-        const chartSettings =
-            {measure: ' '
-            ,resizable: false
-            ,height: 100
-            ,margin: margin};
-        let chartData = [];
-
-        if (d.groups) {
-            chartSettings.panel = 'group';
-            d.groups.forEach(group => {
-                group.values.forEach(value => {
-                    chartData.push({group: group.group, ' ': value});
-                });
-            });
-        } else {
-            d.values.forEach(d => {
-                chartData.push({' ': d});
-            });
-        }
-
-        const chart = createHistogram(chartContainer, chartSettings);
-        chart.init(chartData);
+      makeHistogram(this,d)
     }
 }
