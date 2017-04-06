afddb344b0cb4cf530be2c6165a4cebac7e3e287
Spencer Childress <samussiah@gmail.com>
2017-04-06 15:02:16 -0400

    Update devDependencies to support a `watch` command.`

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { createChart } from './createChart';
 import { createExplorer } from './createExplorer';
 import { createHistogram } from './createHistogram';
 
-
 export default {
   createChart:createChart,
   createExplorer:createExplorer,
