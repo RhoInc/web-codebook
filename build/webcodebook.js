adb0c0dbb3cf97a6946c3ff537858c81e66264a5
Jeremy Wildfire <jwildfire@gmail.com>
2017-04-06 18:33:25 -0700

    updated variable summaries. fixes #27

--- build/webcodebook.js
+++ build/webcodebook.js
@@ -72,10 +72,10 @@ var webcodebook = function (webcharts) {
 		});
 
 		//Clear custom controls.
-		selector.select('ul').remove();
+		selector.selectAll('ul.nav').remove();
 
 		//Add filter controls.
-		var filterList = selector.append('ul');
+		var filterList = selector.append('ul').attr('class', 'nav');
 
 		var filterItem = filterList.selectAll('li').data(chart.config.filters).enter().append('li').attr('class', function (d) {
 			return 'custom-' + d.key + ' filterCustom';
@@ -214,25 +214,86 @@ var webcodebook = function (webcharts) {
 
 	function makeTitle(d) {
 		var wrap = d3.select(this);
+		var titleDiv = wrap.append('div').attr("class", "var-name");
+		var valuesList = wrap.append('ul').attr("class", "value-list");
 
-		var title = wrap.append('div').html(function (d) {
+		//Title and type
+		titleDiv.append("div").attr("class", "name").html(function (d) {
 			return d.value_col;
 		});
+		titleDiv.append("div").attr("class", "type").html(function (d) {
+			return d.type;
+		});
 
-		//add a short summary
-		var summary_text = d.type == 'categorical' ? ' ' + d.type + ' variable with ' + d.statistics.values.length + ' levels' : ' ' + d.type + ' variable';
-		var summary_text_span = title.append('span').attr('class', 'small').text(summary_text);
-
-		if (d.type == 'categorical') {
-			var value_list = d.statistics.values.map(function (m) {
-				return m.key + ': ' + d3.format('0.1%')(m.prop_n);
+		//make a list of values
+		console.log(d);
+		if (d.type == "categorical") {
+			//valuesList.append("span").text( "Values (Most Frequent):")
+			var topValues = d.statistics.values.sort(function (a, b) {
+				return b.n - a.n;
+			}).filter(function (d, i) {
+				return i < 5;
 			});
-			var nValues = value_list.length;
-			value_list = value_list.slice(0, 10).join('\n');
-			value_list = nValues > 10 ? value_list + '\nAnd ' + (nValues - 10) + ' more.' : value_list;
 
-			summary_text_span.append('sup').html('?').style('cursor', 'pointer').attr('title', value_list);
+			valuesList.selectAll("li").data(topValues).enter().append("li").text(function (d) {
+				return d.key + " (" + d3.format("0.1%")(d.prop_n) + ")";
+			}).attr("title", function (d) {
+				return "n=" + d.n;
+			}).style("cursor", "pointer");
+
+			if (d.statistics.values.length > 5) {
+				var totLength = d.statistics.values.length;
+				var extraCount = totLength - 5;
+				var extra_span = valuesList.append("span").html("and " + extraCount + " more.");
+			}
+		} else if (d.type == "continuous") {
+			//valuesList.append("span").text( "Values (Most Frequent):"
+			var sortedValues = d3.set(d.values).values() //get unique
+			.map(function (d) {
+				return +d;
+			}) //convert to numeric
+			.sort(function (a, b) {
+				return a - b;
+			}); // sort low to high
+
+			var minValues = sortedValues.filter(function (d, i) {
+				return i < 3;
+			});
+			var nValues = sortedValues.length;
+			var maxValues = sortedValues.filter(function (d, i) {
+				return i >= nValues - 3;
+			});
+			var valList = d3.merge([minValues, ["..."], maxValues]);
+
+			valuesList.selectAll("li").data(valList).enter().append("li").text(function (d) {
+				return d;
+			}).attr("title", function (d) {
+				return d == "..." ? nValues - 6 + " other values" : "";
+			}).style("cursor", function (d) {
+				return d == "..." ? "pointer" : null;
+			});
 		}
+
+		/*
+  	var summary_text = d.type == 'categorical' ? ' '+d.type + ' variable with '+d.statistics.values.length+' levels' : ' '+d.type + ' variable'
+  	var summary_text_span = title.append('span')
+  	.attr('class','small')
+  	.text(summary_text)
+  
+  	if(d.type == 'categorical'){
+  		var value_list = d.statistics.values.map(m=>m.key+': '+d3.format('0.1%')(m.prop_n))
+  		var nValues = value_list.length;
+  		value_list = value_list.slice(0,10).join('\n')
+  		value_list = nValues > 10 ?
+  			value_list + '\nAnd '+(nValues-10)+' more.':
+  			value_list
+  
+  		summary_text_span.append('sup')
+  		.html('?')
+  		.style('cursor','pointer')
+  		.attr('title',value_list)
+  	}
+  	*/
 	}
 
 	function makeDotPlot(this_, d) {
@@ -867,8 +928,9 @@ var webcodebook = function (webcharts) {
 			return {
 				key: stat !== 'nMissing' ? stat : 'Missing',
 				value: d.statistics[stat] };
+		}).filter(function (statItem) {
+			return ['min', 'max'].indexOf(statItem.key) === -1;
 		});
-		//    .filter(statItem => ['N', 'n'].indexOf(statItem.key) === -1);
 
 		//Render Values
 		if (d.type == 'categorical') {
