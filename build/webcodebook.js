f04db0089b8b32d4cc81d952afb18d9d086005da
Jeremy Wildfire <jwildfire@gmail.com>
2017-04-10 11:52:06 -0700

    change cursor type. fix #40

--- build/webcodebook.js
+++ build/webcodebook.js
@@ -255,7 +255,7 @@ function makeTitle(d) {
 			return d.key + " (" + d3.format("0.1%")(d.prop_n) + ")";
 		}).attr("title", function (d) {
 			return "n=" + d.n;
-		}).style("cursor", "pointer");
+		}).style("cursor", "help");
 
 		if (d.statistics.values.length > 5) {
 			var totLength = d.statistics.values.length;
@@ -286,30 +286,9 @@ function makeTitle(d) {
 		}).attr("title", function (d) {
 			return d == "..." ? nValues - 6 + " other values" : "";
 		}).style("cursor", function (d) {
-			return d == "..." ? "pointer" : null;
+			return d == "..." ? "help" : null;
 		});
 	}
-
-	/*
- 	var summary_text = d.type == 'categorical' ? ' '+d.type + ' variable with '+d.statistics.values.length+' levels' : ' '+d.type + ' variable'
- 	var summary_text_span = title.append('span')
- 	.attr('class','small')
- 	.text(summary_text)
- 
- 	if(d.type == 'categorical'){
- 		var value_list = d.statistics.values.map(m=>m.key+': '+d3.format('0.1%')(m.prop_n))
- 		var nValues = value_list.length;
- 		value_list = value_list.slice(0,10).join('\n')
- 		value_list = nValues > 10 ?
- 			value_list + '\nAnd '+(nValues-10)+' more.':
- 			value_list
- 
- 		summary_text_span.append('sup')
- 		.html('?')
- 		.style('cursor','pointer')
- 		.attr('title',value_list)
- 	}
- 	*/
 }
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
