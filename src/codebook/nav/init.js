b1bce1debf67cdaddcb82d8cef1ad428200deb30
samussiah <samussiah@gmail.com>
2017-07-03 16:25:19 -0400

    merge nav_tabs branch

--- src/codebook/nav/init.js
+++ src/codebook/nav/init.js
@@ -22,30 +22,32 @@ export function init(codebook) {
   });
 
   //draw the nav
-  var chartNav = codebook.nav.wrap.append('ul').attr('class', 'nav nav-tabs');
-  var navItems = chartNav
-    .selectAll('li')
-    .data(codebook.nav.tabs) //make this a setting
-    .enter()
-    .append('li')
-    .attr('class', d => d.key)
-    .classed('active', function(d, i) {
-      return d.active; //make this a setting
-    })
-    .attr('title', d => `View ${d.key}`);
+  if (codebook.nav.tabs.length > 1) {
+    var chartNav = codebook.nav.wrap.append('ul').attr('class', 'nav nav-tabs');
+    var navItems = chartNav
+      .selectAll('li')
+      .data(codebook.nav.tabs) //make this a setting
+      .enter()
+      .append('li')
+      .attr('class', d => d.key)
+      .classed('active', function(d, i) {
+        return d.active; //make this a setting
+      })
+      .attr('title', d => `View ${d.key}`);
 
-  navItems.append('a').html(function(d) {
-    return d.label;
-  });
+    navItems.append('a').html(function(d) {
+      return d.label;
+    });
 
-  //event listener for nav clicks
-  navItems.on('click', function(d) {
-    if (!d.active) {
-      codebook.nav.tabs.forEach(function(t) {
-        t.active = d.label == t.label; //set the clicked tab to active
-        navItems.filter(f => f == t).classed('active', t.active); //style the active nav element
-        t.wrap.classed('hidden', !t.active); //hide all of the wraps (except for the active one)
-      });
-    }
-  });
+    //event listener for nav clicks
+    navItems.on('click', function(d) {
+      if (!d.active) {
+        codebook.nav.tabs.forEach(function(t) {
+          t.active = d.label == t.label; //set the clicked tab to active
+          navItems.filter(f => f == t).classed('active', t.active); //style the active nav element
+          t.wrap.classed('hidden', !t.active); //hide all of the wraps (except for the active one)
+        });
+      }
+    });
+  }
 }
