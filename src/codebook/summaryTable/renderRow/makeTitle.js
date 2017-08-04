7c4bb11b4848f12ced3847d96bd76fa4edd99cb7
Jeremy Wildfire <jwildfire@gmail.com>
2017-08-04 09:25:21 -0700

    partial refactor of codebook header (new file)

---
+++ src/codebook/summaryTable/renderRow/makeTitle.js
@@ -0,0 +1,27 @@
+import { select as d3select } from 'd3';
+
+export default function makeTitle(d) {
+  d3select(this)
+    .append('div')
+    .attr('class', 'row-toggle')
+    .html('&#9660;')
+    .on('click', function() {
+      var rowDiv = d3select(this.parentNode.parentNode.parentNode);
+      var chartDiv = rowDiv.select('.row-chart');
+      var hiddenFlag = rowDiv.classed('hiddenChart');
+      rowDiv.classed('hiddenChart', !hiddenFlag);
+      d3select(this).html(hiddenFlag ? '&#9660;' : '&#9658;');
+    });
+
+  d3select(this)
+    .append('span')
+    .attr('class', 'title-span')
+    .text(d => d.value_col);
+
+  if (d.value_col != d.label) {
+    d3select(this)
+      .append('span')
+      .attr('class', 'label-span')
+      .text(d => d.label);
+  }
+}
