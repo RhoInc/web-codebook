ac516e565d0a35e0e628685f60151da54339758b
Jeremy Wildfire <jwildfire@gmail.com>
2017-08-23 07:45:11 -0700

    clear console.logs

--- src/explorer/addFiles.js
+++ src/explorer/addFiles.js
@@ -4,13 +4,10 @@ export function addFiles(files) {
   var newFiles = files.filter(function(f) {
     return explorer.config.files.indexOf(f) == -1;
   });
-  console.log(newFiles);
+
   //add new files to file list
   this.config.files = d3.merge([this.config.files, newFiles]);
 
   //re-draw the file listing
   explorer.codebook.fileListing.table.draw(this.config.files);
-
-  console.log('added files?');
-  console.log(explorer);
 }
