92b0d8f2e325208bcc193757b864d451d5af206f
jwildfire <jwildfire@gmail.com>
2018-04-11 11:45:20 -0700

    add metadata as tags

---
+++ src/codebook/summaryTable/renderRow/makeMeta.js
@@ -0,0 +1,17 @@
+import { select as d3select } from 'd3';
+import renderMeta from './details/renderMeta';
+
+export default function makeMeta(d) {
+  var hasMeta =
+    d.meta.filter(f => !f.hidden).filter(f => f.key != 'Type').lenght > 0;
+  if (hasMeta) {
+    var meta_list = d3select(this)
+      .append('ul')
+      .attr('class', 'meta');
+
+    var parent = d3select(this.parentNode.parentNode);
+    renderMeta(d, meta_list);
+  } else {
+    d3select(this).style('display', 'none');
+  }
+}
