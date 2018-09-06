a9166d95968ac69b232328dcc9cca2e0cb3e8386
jwildfire <jwildfire@gmail.com>
2018-09-06 12:06:02 -0700

    restructure test pages. fix #297

---
+++ test-page/index.js
@@ -0,0 +1,27 @@
+document.onreadystatechange = function () {
+    if (document.readyState === 'complete') {
+        d3.csv(
+            'https://rawgit.com/RhoInc/viz-library/master/data/testData/ADTIMELINES_partialMissing.csv',
+            function(error,data) {
+                if (error)
+                    console.log(error);
+
+                var settings = {
+                //    chartVisibility: 'hidden',
+                    meta:[
+                    {
+                        value_col:"USUBJID",
+                        label:"Subject ID",
+                        description: "Unique Identifier"
+                    }
+                    ]
+                };
+                var instance = webcodebook.createCodebook(
+                    '#container',
+                    settings
+                );
+                instance.init(data);
+            }
+        );
+    };
+}
