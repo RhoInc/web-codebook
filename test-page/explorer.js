99b065daea7f660fe30d20faeb522b836c7bf80e
jwildfire <jwildfire@gmail.com>
2018-09-06 13:03:48 -0700

    run prettier on test pages

--- test-page/explorer.js
+++ test-page/explorer.js
@@ -1,38 +1,43 @@
-document.onreadystatechange = function () {
-        function initExplorer(fileList){
-        var metaFiles = ["AE","DM","LB"] //list of files to add meta data
-        d3.csv("https://rawgit.com/RhoInc/viz-library/master/data/safetyData/variableMetaData.csv",function(error,meta){
-            meta.forEach(function(f){
-            f.file = f.Form+".csv"
-            f.label = f.Label
-            })
-            var fileList_clean = fileList.map(function(f){
-                f.path = "https://rawgit.com/RhoInc/viz-library/master"+f.rel_path.slice(1);
-            f.shortname = f.filename.replace(/\.[^/.]+$/, "")
-            if(metaFiles.indexOf(f.shortname)>-1){
-                f.meta = meta.filter(m=>m.Form == f.shortname)
-                .map(function(m){
-                m.value_col = m.Variable;
-                return m;
-                })
-                f["Metadata included?"] = "Yes"
-            }else{
-                f["Metadata included?"] = "No"
-            }
-                return f;
-            })
+document.onreadystatechange = function() {
+  function initExplorer(fileList) {
+    var metaFiles = ['AE', 'DM', 'LB']; //list of files to add meta data
+    d3.csv(
+      'https://rawgit.com/RhoInc/viz-library/master/data/safetyData/variableMetaData.csv',
+      function(error, meta) {
+        meta.forEach(function(f) {
+          f.file = f.Form + '.csv';
+          f.label = f.Label;
+        });
 
-            var settings = {
-                labelColumn:"filename",
-                ignoredColumns: ["local_path","rel_path","path"],
-                files:fileList_clean,
-                meta:meta,
-                fileLoader:true
-            };
-            var explorer = webcodebook.createExplorer("#container", settings).init();
-        })
+        var fileList_clean = fileList.map(function(f) {
+          f.path =
+            'https://rawgit.com/RhoInc/viz-library/master' +
+            f.rel_path.slice(1);
+          f.shortname = f.filename.replace(/\.[^/.]+$/, '');
+          if (metaFiles.indexOf(f.shortname) > -1) {
+            f.meta = meta.filter(m => m.Form == f.shortname).map(function(m) {
+              m.value_col = m.Variable;
+              return m;
+            });
+            f['Metadata included?'] = 'Yes';
+          } else {
+            f['Metadata included?'] = 'No';
+          }
+          return f;
+        });
+        var settings = {
+          labelColumn: 'filename',
+          ignoredColumns: ['local_path', 'rel_path', 'path'],
+          files: fileList_clean,
+          meta: meta,
+          fileLoader: true
+        };
+        var explorer = webcodebook
+          .createExplorer('#container', settings)
+          .init();
+      }
+    );
+  }
 
-        }
-
-        initExplorer(dataFiles)
-}
+  initExplorer(dataFiles);
+};
--- test-page/index.js
+++ test-page/index.js
@@ -1,27 +1,23 @@
-document.onreadystatechange = function () {
-    if (document.readyState === 'complete') {
-        d3.csv(
-            'https://rawgit.com/RhoInc/viz-library/master/data/testData/ADTIMELINES_partialMissing.csv',
-            function(error,data) {
-                if (error)
-                    console.log(error);
+document.onreadystatechange = function() {
+  if (document.readyState === 'complete') {
+    d3.csv(
+      'https://rawgit.com/RhoInc/viz-library/master/data/testData/ADTIMELINES_partialMissing.csv',
+      function(error, data) {
+        if (error) console.log(error);
 
-                var settings = {
-                //    chartVisibility: 'hidden',
-                    meta:[
-                    {
-                        value_col:"USUBJID",
-                        label:"Subject ID",
-                        description: "Unique Identifier"
-                    }
-                    ]
-                };
-                var instance = webcodebook.createCodebook(
-                    '#container',
-                    settings
-                );
-                instance.init(data);
+        var settings = {
+          //    chartVisibility: 'hidden',
+          meta: [
+            {
+              value_col: 'USUBJID',
+              label: 'Subject ID',
+              description: 'Unique Identifier'
             }
-        );
-    };
-}
+          ]
+        };
+        var instance = webcodebook.createCodebook('#container', settings);
+        instance.init(data);
+      }
+    );
+  }
+};
