ca520e9dda77d67ec90172718b27ba099392c280
samussiah <samussiah@gmail.com>
2018-10-24 13:30:09 -0400

    point test-page to remote data location

--- test-page/index.js
+++ test-page/index.js
@@ -1,28 +1,30 @@
-document.onreadystatechange = function() {
-  if (document.readyState === 'complete') {
-    d3.csv('./ADTIMELINES_partialMissing.csv', function(error, data) {
-      if (error) console.log(error);
+d3.csv(
+  'https://rawgit.com/RhoInc/viz-library/master/data/testData/ADTIMELINES_partialMissing.csv',
+  function(d) {
+    return d;
+  },
+  function(error, data) {
+    if (error) console.log(error);
 
-      var settings = {
-        //    chartVisibility: 'hidden',
-        //autogroups: 3,
-        groups: ['SEX'],
-        meta: [
-          {
-            value_col: 'USUBJID',
-            label: 'Subject ID',
-            description: 'Unique Identifier',
-            type: 'categorical'
-          },
-          {
-            value_col: 'SITEID',
-            label: 'Site ID',
-            type: 'continuous'
-          }
-        ]
-      };
-      var instance = webcodebook.createCodebook('#container', settings);
-      instance.init(data);
-    });
+    var settings = {
+      //        chartVisibility: 'hidden',
+      //autogroups: 3,
+      groups: ['SEX'],
+      meta: [
+        {
+          value_col: 'USUBJID',
+          label: 'Subject ID',
+          description: 'Unique Identifier',
+          type: 'categorical'
+        },
+        {
+          value_col: 'SITEID',
+          label: 'Site ID',
+          type: 'continuous'
+        }
+      ]
+    };
+    var instance = webcodebook.createCodebook('#container', settings);
+    instance.init(data);
   }
-};
+);
